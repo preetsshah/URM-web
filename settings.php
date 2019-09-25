66fdd865060d2cf5d6c8dbbae6c133fe2afa9117
preetsshah <preetshah0408@gmail.com>
2019-09-25 15:12:54 +0530

    composer auth file

--- settings.php
+++ settings.php
@@ -4,5 +4,5 @@ define('CLIENT_ID', '201589853547-na1i8d7t6urb9prsro69vio9mjcf8c39.apps.googleus
 /* Google App Client Secret */
 define('CLIENT_SECRET', 'vYMZrrPRp3KAwQz9HQlUOf_P');
 /* Google App Redirect Url */
-define('CLIENT_REDIRECT_URL', 'http://localhost:85/urm-web/home.php');
+define('CLIENT_REDIRECT_URL', 'http://localhost/urm-web/home.php');
 ?>
