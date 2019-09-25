e9fc49733364fbfdadf5c35393561e975abf6374
Priyank Shah <priyankshah9840@gmail.com>
2019-09-25 14:31:38 +0530

    new logo ji - google kaw woh bhi

--- index.php
+++ index.php
@@ -2,16 +2,16 @@
 require_once('settings.php');
 ?>
 <!doctype html>
-<html lang=""">
+<html lang="" xmlns:text-transform="http://www.w3.org/1999/xhtml"/>
 <head>
     <meta charset="utf-8">
-    <title>Bootstrap 4 template</title>
+    <title>University Resource Management</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="apple-touch-icon" href="">
     <link rel="shortcut icon" href="" type="image/x-icon">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/booktstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="">
 </head>
@@ -21,20 +21,18 @@ require_once('settings.php');
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
 <!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+
 <div class="row">
-    <div class="btn white darken-4 col-md-5 s10 m4" style="margin:auto;">
-        <a href="<?= 'https://accounts.google.com/o/oauth2/auth?scope=' . urlencode('https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email') . '&redirect_uri=' . urlencode(CLIENT_REDIRECT_URL) . '&response_type=code&client_id=' . CLIENT_ID . '&access_type=online' ?>"
-           style="text-transform:none">
-            <div class="left">
-                <img width="20px" alt="Google &quot;G&quot; Logo"
-                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png"/>
+    <div class="btn white darken-4" style="margin:auto;cursor: pointer;width: auto" onclick='window.location="<?= 'https://accounts.google.com/o/oauth2/auth?scope=' . urlencode('https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email') . '&redirect_uri=' . urlencode(CLIENT_REDIRECT_URL) . '&response_type=code&client_id=' . CLIENT_ID . '&access_type=online' ?>"'>
+            <div class="center">
+                <img width="20px" alt="Google Logo"
+                     src="./public_html/image/content/google_logo.png"/>&nbsp;
+                <span style="color: dodgerblue;font-weight: bold">Login with Google</span>
             </div>
-            Login with Google
-        </a>
+
     </div>
 </div>
 
-<!--<button onclick="googleSignIn()">Google SignIn</button>-->
 <script src="https://www.gstatic.com/firebasejs/3.2.0/firebase.js"></script>
 <script src="auth.js"></script>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
